0767bcacc88e388701af06fa6f21d22e1325a9e2
Wolodja <wolodjawaskovych@i.ua>
2020-10-08 17:08:29 +0200

    Select decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { INCREMENT } from './actions';
-import { NgRedux } from '@angular-redux/store';
+import { NgRedux, select } from '@angular-redux/store';
 import { Component } from '@angular/core';
 import { IAppState } from './store';
 
@@ -10,10 +10,11 @@ import { IAppState } from './store';
 })
 export class AppComponent {
   title = 'redux-demo';
-  counter = 0;
+  @select('counter') counter;
+  @select(['messaging', 'newMessaging']) newMessages;
+  @select((s: IAppState) => s.messaging.newMessages) newMessagesCount;
 
   constructor(private ngRedux: NgRedux<IAppState>) {
-
   }
 
   increment() {
